f47e7272bf03760bfc2bd0a7760ee2438a2fce2f
Marco Verrocchio <marco.verrocchio@cineca.it>
2015-07-17 10:57:46 +0200

    caching multi-user

--- Memoize.py
+++ Memoize.py
@@ -3,25 +3,27 @@ import datetime
 class Memoize:
     def __init__(self, f):
         self.f = f
-        self.memo = {}
+        self.cache = {}
         self.callTime = {}
 
     def __call__(self, *args):
+        cookie, url, surname = args
+        surname = surname.upper(surname)
+        keyArgs = (url,surname)
         callTime = datetime.datetime.today()
-        print "CALL TIME IS: ",callTime
-        if not args in self.memo:
+
+        if not keyArgs in self.cache:
             print "NEW ARGS ... MAKE REAL CALL"
-            self.memo[args] = self.f(*args)
-            self.callTime[args] = callTime
+            self.cache[keyArgs] = self.f(*args)
+            self.callTime[keyArgs] = callTime
         else:
-            precCallTime = self.callTime[args]
-            delta = callTime-precCallTime
-            delta = delta.total_seconds()
-            if delta > 60:
+            precCallTime = self.callTime[keyArgs]
+            delta = (callTime-precCallTime).total_seconds()
+            if delta > 1800:
                 print "OLD ARGS BUT TIME LIMIT REACHED ... MAKE REAL CALL"
-                self.memo[args] = self.f(*args)
-                self.callTime[args] = callTime
+                self.cache[keyArgs] = self.f(*args)
+                self.callTime[keyArgs] = callTime
             else:
                 print "RETURN CACHED"
-        return self.memo[args]
+        return self.cache[keyArgs]
 
